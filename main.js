441b6bb78b17da8f6251eb95f172868334519ff7
Chris Nelson <chrisnelson7791@gmail.com>
2024-06-08 11:26:12 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function boolToWord( bool ){
+
+  if (bool === true) return 'Yes'
+  if (bool === false) return 'No'
+  //...
+}
+
+// params: bool   // a boolean value
+// return: str    // boolean converted to a string
+// e.g. boolToWord(true)
+// results in "Yes"
+
+
+// conditional if bool is true return 'Yes', if it's false return 'No'
+
+
+module.exports = boolToWord
